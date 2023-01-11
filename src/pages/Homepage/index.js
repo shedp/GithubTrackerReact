71c647d5f912c2f42a66c91ca9a4696ac53cdd16
“shedp” <sheenadiep@gmail.com>
2023-01-11 12:27:35 +0000

    created homepage

---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import SearchForm from '../../components/SearchForm';
+import Result from '../../components/Result';
+import {getResult} from '../../actions';
+
+const Search = () => {
+    const result = useSelector(state => state.result)
+    const loading = useSelector(state => state.loading)
+    const error = useSelector(state => state.error)
+    const dispatch = useDispatch();
+
+    
+    
+    const renderResult = () => {
+        return loading ? <p>Loading...</p> : <Result result={result}/>
+    }
+
+    const search = searchTerm => dispatch(getResult(searchTerm))
+
+    return(
+        <>
+            <div id="search">
+                <SearchForm getResult={search}/>
+                
+                {error ? <p role="alert"> Oops {error}</p> : renderResult()}
+            </div>
+            
+        </>
+    )
+};
+
+export default Search;
