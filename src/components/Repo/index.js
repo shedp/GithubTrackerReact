dfb18ebbb313bf49be21c828e99ff7774948516e
“shedp” <sheenadiep@gmail.com>
2023-01-11 15:35:26 +0000

    add icons to repo

--- src/components/Repo/index.js
+++ src/components/Repo/index.js
@@ -7,9 +7,10 @@ const Repo = ({id}) => {
     return ( 
         <div className="repo-info">
             {repo && <h1>{repo.name}</h1>}
-            {repo && <p>Stargazers {repo.stargazers_count}</p>}
-            {repo && <p>Forks {repo.forks_count}</p>}
-            {repo && <p>Issues {repo.open_issues_count}</p>}
+            <br/>
+            {repo && <div className="repo-item"><svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-star v-align-text-bottom d-inline-block mr-2"><path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"></path></svg> <p>Stars:  {repo.stargazers_count}</p></div>}
+            {repo && <div className="repo-item"><svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-repo-forked mr-2"><path fill-rule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path></svg><p>Forks:  {repo.forks_count}</p></div>}
+            {repo && <div className="repo-item"><svg aria-hidden="true" height="15" viewBox="0 0 24 24" version="1.1" width="15" data-view-component="true" class="octicon octicon-issue-opened blankslate-icon"><path fill-rule="evenodd" d="M2.5 12a9.5 9.5 0 1119 0 9.5 9.5 0 01-19 0zM12 1C5.925 1 1 5.925 1 12s4.925 11 11 11 11-4.925 11-11S18.075 1 12 1zm0 13a2 2 0 100-4 2 2 0 000 4z"></path></svg> <p>Issues: {repo.open_issues_count}</p></div>}
         </div>
      );
 }
