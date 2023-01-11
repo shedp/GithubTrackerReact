b4cce581da5c76475d2bba7f1a316a9d4945e353
“shedp” <sheenadiep@gmail.com>
2023-01-11 17:57:01 +0000

    added placeholders

---
+++ src/components/Placeholder/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Placeholder = () => {
+    return ( 
+        <div className="Placeholder">
+            <img src="logo.png" width="180px" height="180px"></img>
+            <h1>Search for a user</h1>
+        </div>
+     );
+}
+ 
+export default Placeholder;
---
+++ src/components/PlaceholderAvatar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import lightlogo from './lightlogo.png';
+
+const PlaceholderAvatar = () => {
+    return ( 
+        <div className="mini-avatar placeholder">
+            <img src="lightlogo.png" width="18px" height="18px"/>
+        </div>
+     );
+}
+ 
+export default PlaceholderAvatar;
