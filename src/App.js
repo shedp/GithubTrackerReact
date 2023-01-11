fdf33e30092ababdb322e01f0cac64f49cecc190
“shedp” <sheenadiep@gmail.com>
2023-01-11 12:24:17 +0000

    adding repo page to homepage

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Routes ,Route } from 'react-router-dom';
+import Navbar from "./components/navbar";
+import {Homepage} from "./pages";
+
+import './styles/styles.css'
+import './styles/navbar.css'
+import './styles/homepage.css'
+
+const App = () => {
+    return(
+        <>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Homepage/>}/>
+                {/* <Route exact path="/repo/:id" element={<Repo/>}/> */}
+            </Routes>
+            
+        </>
+    );
+    
+}
+export default App;
