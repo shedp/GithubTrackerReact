7f198780e6e57206a66d934eced47401aa02cc7e
“shedp” <sheenadiep@gmail.com>
2023-01-13 12:18:54 +0000

    changes to avatar rendering

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -3,11 +3,10 @@ import {NavLink} from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
 const Avatar = ({result}) => {
-    
     return (
         <div className="mini-avatar">
             {result && 
-                <a href={result[0].owner.html_url}><img src={result[0].owner.avatar_url}/></a>
+                <a href={result.html_url}><img src={result.avatar_url}/></a>
             }
         </div>
     )
