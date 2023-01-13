facad459e90ed7f0715d0096b5e9f44039b52ad1
“shedp” <sheenadiep@gmail.com>
2023-01-13 12:19:39 +0000

    restructuring repos

--- src/components/Repo/index.js
+++ src/components/Repo/index.js
@@ -3,23 +3,37 @@ import { useSelector } from "react-redux";
 import dayjs from "dayjs";
 
 const Repo = ({id}) => {
-    const repo = useSelector(state => state.result.find(r => r.id == id ))
-    console.log(repo)
+    const repo = useSelector(state => state.repos.find(r => r.id == id ))
+    
     return ( 
         <div className="repo-info">
-            {repo && <a href={repo.html_url}><h1>{repo.name}</h1></a>}
-            <br/>
-            {repo.description && <p>Description: {repo.description}</p>}
-            <br/>
-            {repo && <div className="repo-item"><img src="./images/repo-star.svg" alt="" /> <p>Stars:  {repo.stargazers_count}</p></div>}
-            {repo && <div className="repo-item"><img src="./images/repo-fork.svg" alt="" /><p>Forks:  {repo.forks_count}</p></div>}
-            {repo && <div className="repo-item"><img src="./images/repo-issue.svg" alt="" /> <p>Issues: {repo.open_issues_count}</p></div>}
-            {repo && <div className="repo-item"><img src="./images/repo-watcher.svg" alt="" /> <p>Watchers: {repo.watchers_count}</p></div>}
-            <br/>
-            {repo && <p>Created: {dayjs(repo.created_at).format("DD/MM/YYYY")}</p>}
-            {repo && <p>Last Updated: {dayjs(repo.updated_at).format("DD/MM/YYYY")}</p>}
+            {repo && <div>
+                <a href={repo.html_url}><h1>{repo.name}</h1></a>
+                <br/>
+                {repo.description && <p>Description: {repo.description}</p>}
+                <br/>
+                <div className="repo-item">
+                    <img src="./images/repo-star.svg" alt="" /> 
+                    <p>Stars:  {repo.stargazers_count}</p>
+                </div>
+                <div className="repo-item">
+                    <img src="./images/repo-fork.svg" alt="" />
+                    <p>Forks:  {repo.forks_count}</p>
+                </div>
+                <div className="repo-item">
+                    <img src="./images/repo-issue.svg" alt="" /> 
+                    <p>Issues: {repo.open_issues_count}</p>
+                </div>
+                <div className="repo-item">
+                    <img src="./images/repo-watcher.svg" alt="" /> 
+                    <p>Watchers: {repo.watchers_count}</p>
+                </div>
+                <br/>
+                <p>Created: {dayjs(repo.created_at).format("DD/MM/YYYY")}</p>
+                <p>Last Updated: {dayjs(repo.updated_at).format("DD/MM/YYYY")}</p>
+            </div>
 
-        </div>
+            }</div>
      );
 }
  
